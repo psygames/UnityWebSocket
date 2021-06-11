ea443e70964f989ae164c15a6a727d96e5bdd1b7
yinlong <799329256@qq.com>
2021-06-11 17:56:56 +0800

    http server

--- Assets/UnityWebSocket/Scripts/Editor/TestServerWindow.cs
+++ Assets/UnityWebSocket/Scripts/Editor/TestServerWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 using WebSocketSharp.Server;
@@ -31,12 +32,12 @@ namespace UnityWebSocket.Editor
             }
         }
 
-        private WebSocketServer server;
+        private HttpServer server;
         private List<string> logs = new List<string>();
         private Vector2 scroll;
         private bool needRepaint;
         private int port = 5963;
-        private bool secure = false;
+        private bool secure = true;
 
         private void OnGUI()
         {
@@ -69,15 +70,51 @@ namespace UnityWebSocket.Editor
                 GUI.color = Color.green;
                 if (GUILayout.Button("Start", GUILayout.Height(30)))
                 {
-                    server = new WebSocketServer(port, secure);
+                    server = new HttpServer(port, secure);
+                    server.Log.Level = WebSocketSharp.LogLevel.Trace;
                     if (secure)
                     {
                         var certPath = "Assets/UnityWebSocket/Scripts/Editor/cert.pfx";
                         var certPwd = "123456";
                         server.SslConfiguration.ServerCertificate =
                             new X509Certificate2(certPath, certPwd);
-                        server.SslConfiguration.EnabledSslProtocols = (SslProtocols)4092;
+                        server.SslConfiguration.EnabledSslProtocols =
+                        SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Ssl2;
                     }
+                    // Set the document root path.
+                    server.DocumentRootPath = "Assets/UnityWebSocket/Scripts/Editor/";
+
+                    // Set the HTTP GET request event.
+                    server.OnGet += (sender, e) =>
+                    {
+                        var req = e.Request;
+                        var res = e.Response;
+
+                        var path = req.RawUrl;
+                        if (path == "/")
+                            path += "index.html";
+
+                        byte[] contents;
+                        if (!e.TryReadFile(path, out contents))
+                        {
+                            res.StatusCode = (int)WebSocketSharp.Net.HttpStatusCode.NotFound;
+                            return;
+                        }
+
+                        if (path.EndsWith(".html"))
+                        {
+                            res.ContentType = "text/html";
+                            res.ContentEncoding = Encoding.UTF8;
+                        }
+                        else if (path.EndsWith(".js"))
+                        {
+                            res.ContentType = "application/javascript";
+                            res.ContentEncoding = Encoding.UTF8;
+                        }
+
+                        res.ContentLength64 = contents.LongLength;
+                        res.Close(contents, true);
+                    };
                     server.AddWebSocketService<TestServer>("/");
                     server.Start();
                 }
@@ -120,10 +157,15 @@ namespace UnityWebSocket.Editor
             protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
             {
                 if (e.IsBinary)
+                {
                     Log(ID + ": Received: bytes(" + e.RawData.Length + ")");
+                    Send(e.RawData);
+                }
                 else
+                {
                     Log(ID + ": Received: " + e.Data + "");
-                Send(e.RawData);
+                    Send(e.Data);
+                }
             }
 
             protected override void OnClose(WebSocketSharp.CloseEventArgs e)
