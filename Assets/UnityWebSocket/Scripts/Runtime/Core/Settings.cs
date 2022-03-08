f8ecb6b03b77ac2279540459fa610746d1d221cf
psygames <p>
2022-03-08 18:02:24 +0800

    settings version 2.6.4

--- Assets/UnityWebSocket/Scripts/Runtime/Core/Settings.cs
+++ Assets/UnityWebSocket/Scripts/Runtime/Core/Settings.cs
@@ -1,4 +1,4 @@
-﻿namespace UnityWebSocket
+namespace UnityWebSocket
 {
     public static class Settings
     {
@@ -7,6 +7,6 @@
         public const string QQ_GROUP_LINK = "https://qm.qq.com/cgi-bin/qm/qr?k=KcexYJ9aYwogFXbj2aN0XHH5b2G7ICmd";
         public const string EMAIL = "799329256@qq.com";
         public const string AUHTOR = "psygames";
-        public const string VERSION = "2.6.3";
+        public const string VERSION = "2.6.4";
     }
 }
